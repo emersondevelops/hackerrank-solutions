e931a6f47f9c5c4e9daf3087e0442918eff74944
Emerson Bezerra <emersondevelops@gmail.com>
2020-08-13 21:30:53 -0300

    Adicionar arquivos de projeto.

---
+++ SolveMeFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SolveMeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
